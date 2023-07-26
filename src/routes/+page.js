70f50e0f7d7204f1be186492e53d50b23f597ca4
Robin <robin.leonard@gmail.com>
2023-07-26 16:09:52 +0100

    removed further readme ref

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,15 +1,15 @@
-import { error } from '@sveltejs/kit'
-
-export const load = async () => {
-	try {
-		const ReadMeFile = await import('../../README.md')
-		const ReadMe = ReadMeFile.default
-		
-		return {
-			ReadMe
-		}
-	}
-	catch(err) {
-		throw error(500, err)
-	}
-}
+// import { error } from '@sveltejs/kit'
+y
+// export const load = async () => {
+// 	try {
+// 		const ReadMeFile = await import('../../README.md')
+// 		const ReadMe = ReadMeFile.default
+//
+// 		return {
+// 			ReadMe
+// 		}
+// 	}
+// 	catch(err) {
+// 		throw error(500, err)
+// 	}
+// }
